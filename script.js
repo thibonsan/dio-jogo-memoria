e39b0379c2be25f28ff490a8279e7604e92d4e79
Thiago Bonetti dos Santos <thibonsan@gmail.com>
2022-12-27 19:56:00 +0000

    Parte 6.

--- script.js
+++ script.js
@@ -4,8 +4,12 @@ let firstCard, secondCard;
 let blockBoard = false;
 
 function flipCard() {
-    if (blockBoard) return;
-    
+    if (blockBoard) 
+        return;
+
+    if (this === firstCard) 
+        return;
+
     this.classList.add('flip');
 
     if (!hasFlippedCard) {
@@ -31,15 +35,24 @@ function checkForMatch() {
 function disableCards() {
     firstCard.removeEventListener('click', flipCard);
     secondCard.removeEventListener('click', flipCard);
+    resetBoard();
 }
 
 function unflipCards() {
+    blockBoard = true;
+
     setTimeout(() => {
         firstCard.classList.remove('flip');
         secondCard.classList.remove('flip');
+        resetBoard();
     }, 1500);
+}
+
+function resetBoard() {
+    [hasFlippedCard, blockBoard] = [false, false];
+    [firstCard, secondCard] = [null, null];
 }
 
 cards.forEach((card) => {
     card.addEventListener('click', flipCard);
-})
+});
