9b6e013926fb99935e9055534be1beb8c50f8f8a
Thiago Bonetti dos Santos <thibonsan@gmail.com>
2022-12-27 16:41:49 +0000

    Parte 4.

--- script.js
+++ script.js
@@ -1,7 +1,28 @@
 const cards = document.querySelectorAll('.card');
+let hasFlippedCard = false;
+let firstCard, secondCard;
 
 function flipCard() {
-    this.classList.toggle('flip');
+    this.classList.add('flip');
+
+    if (!hasFlippedCard) {
+        hasFlippedCard = true;
+        firstCard = this;
+        return;
+    }
+
+    secondCard = this;
+    hasFlippedCard = false;
+    checkForMatch();
+}
+
+function checkForMatch() {
+    if (firstCard.dataset.card === secondCard.dataset.card) {
+        disableCards();
+        return;
+    }
+
+    unflipCards();
 }
 
 cards.forEach((card) => {
